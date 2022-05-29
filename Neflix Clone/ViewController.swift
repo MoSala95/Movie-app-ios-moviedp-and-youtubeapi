3e6aae6adf2c0cfdade02455307bd7043e214783
MoSala95 <47943495+MoSala95@users.noreply.github.com>
2022-05-29 18:04:30 +0200

    Initial Commit

---
+++ Neflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neflix Clone
+//
+//  Created by mohamed salah on 29/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
